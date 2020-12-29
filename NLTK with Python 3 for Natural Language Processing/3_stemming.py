c236146a3bbbd50bef0b885915f610aca74767ad
Callum Wright <wrightci01w@gmail.com>
2020-12-29 11:34:56 +0000

    NLTK with Python 3 for Natural Language Processing 3/21

---
+++ NLTK with Python 3 for Natural Language Processing/3_stemming.py
@@ -0,0 +1,16 @@
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+
+ps = PorterStemmer()
+
+example_words = ["Python", "Pythoner", "Pythoning", "Pythoned", "Pythonly"]
+
+# for w in example_words:
+#     print(ps.stem(w))
+
+new_text = "It is very important to be pythonly while pythoning with python. All pythoners have pythoned poorly atleast once."
+
+words = word_tokenize(new_text)
+
+for w in words:
+    print(ps.stem(w))
