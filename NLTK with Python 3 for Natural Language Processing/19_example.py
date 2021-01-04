1ca9ef305232296ae6505eff972dff42f8ecea47
Callum Wright <wrightci01w@gmail.com>
2021-01-04 12:18:45 +0000

    NLTK with Python 3 for Natural Language Processing 19/21

---
+++ NLTK with Python 3 for Natural Language Processing/19_example.py
@@ -0,0 +1,4 @@
+s = __import__("19_sentiment_mod")
+
+print(s.sentiment("This movie was awesome! The acting was great, plot was wonderful, and there were pythons...so yea!"))
+print(s.sentiment("This movie was utter junk. There were absolutely 0 pythons. I don't see what the point was at all. Horrible movie, 0/10"))
